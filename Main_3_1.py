f506887e9357bb93b1e964e40caa0ba55a4a29d8
barifrah1 <63462394+barifrah1@users.noreply.github.com>
2021-04-30 11:51:48 +0300

    fixes to X_meaned to X_normalized

--- Main_3_1.py
+++ Main_3_1.py
@@ -10,15 +10,16 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d
 from matplotlib.patches import FancyArrowPatch
 
-def myPca(X,num_components):
-    #X_normalized = StandardScaler().fit_transform(X)
+
+def myPca(X, num_components):
+    X_normalized = StandardScaler().fit_transform(X)
     # calculate the mean of each column
     Mean_col = np.mean(X, axis=0)
     Std_col = np.std(X, axis=0)
     # center columns by subtracting column means
-    X_meaned = X - Mean_col
+    #X_meaned = X - Mean_col
     # calculate covariance matrix
-    cov_mat = np.cov(X_meaned, rowvar=False)
+    cov_mat = np.cov(X_normalized, rowvar=False)
 
     # eigendecomposition of covariance matrix
     eigen_values, eigen_vectors = np.linalg.eigh(cov_mat)
@@ -30,48 +31,55 @@ def myPca(X,num_components):
     eigenvalue_subset = sorted_eigenvalue[0:num_components]
     eigenvector_subset = sorted_eigenvectors[:, 0:num_components]
 
-    #In the assigment requested to compute the spectral decomposition
+    # In the assigment requested to compute the spectral decomposition
     D_matrix = eigenvalue_subset*np.identity(num_components)
     A_matrix = eigenvector_subset@D_matrix@eigenvector_subset.T
 
     # Get the new projected data  pcaData = normalizedData * projectionVectors
-    X_projected = np.dot(eigenvector_subset.transpose(), X_meaned.transpose()).transpose()
+    X_projected = np.dot(eigenvector_subset.transpose(),
+                         X_normalized.transpose()).transpose()
 
-    return Mean_col, Std_col, X_meaned, eigenvalue_subset, eigenvector_subset, X_projected
+    return Mean_col, Std_col, X_normalized, eigenvalue_subset, eigenvector_subset, X_projected
 
 
-data = load_wine()
+if __name__ == "__main__":
+    data = load_wine()
 
-Mean_col, Std_col, X_meaned, eigenvalues, eigenvectors, X_projected = myPca(data.data,3)
+    Mean_col, Std_col, X_normalized, eigenvalues, eigenvectors, X_projected = myPca(
+        data.data, 3)
 
-# Creating a Pandas DataFrame of reduced Dataset
-principal_df = pd.DataFrame(X_projected, columns=['PC1', 'PC2', 'PC3'])
+    # Creating a Pandas DataFrame of reduced Dataset
+    principal_df = pd.DataFrame(X_projected, columns=['PC1', 'PC2', 'PC3'])
 
-# Concat it with target variable to create a complete Dataset
-principal_df = pd.concat([principal_df, pd.DataFrame(data.target, columns =['target'])], axis=1)
+    # Concat it with target variable to create a complete Dataset
+    principal_df = pd.concat([principal_df, pd.DataFrame(
+        data.target, columns=['target'])], axis=1)
 
-#Plot the 2d PC's
-plt.figure(figsize = (6,6))
-sb.scatterplot(data = principal_df , x = 'PC1',y = 'PC2', hue = 'target' , s = 60 , palette= 'icefire')
-#plt.show()
+    # Plot the 2d PC's
+    plt.figure(figsize=(6, 6))
+    sb.scatterplot(data=principal_df, x='PC1', y='PC2',
+                   hue='target', s=60, palette='icefire')
+    # plt.show()
 
-#PCA reconstruction=PC scores⋅Eigenvectors.T+Mean
-reconstructed_X = X_projected@eigenvectors.T + Mean_col
+    # PCA reconstruction=PC scores⋅Eigenvectors.T+Mean
+    reconstructed_X = (X_projected@eigenvectors.T)*Std_col + Mean_col
 
-#Plot 3d graph for reconstruction plot for column #4,#5 & #6
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
-ax.view_init(15, -60)
+    # Plot 3d graph for reconstruction plot for column #4,#5 & #6
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(15, -60)
 
-recon_class = reconstructed_X
-original_class = data.data
-ax.scatter(recon_class[:, 4], recon_class[:, 5], recon_class[:, 6], color='red', alpha=0.6, label='reconstructed data')
-ax.scatter(original_class[:, 4], original_class[:, 5], original_class[:, 6], color='green', alpha=0.6, label='Original data')
+    recon_class = reconstructed_X
+    original_class = data.data
+    ax.scatter(recon_class[:, 4], recon_class[:, 5], recon_class[:, 6],
+               color='red', alpha=0.6, label='reconstructed data')
+    ax.scatter(original_class[:, 4], original_class[:, 5],
+               original_class[:, 6], color='green', alpha=0.6, label='Original data')
 
-# chart
-plt.title("3 columns of original and reconstructed data of Wine Quality")
-ax.set_xlabel('Col#4')
-ax.set_ylabel('Col#5')
-ax.set_zlabel('Col#6')
-plt.legend()
-
+    # chart
+    plt.title("3 columns of original and reconstructed data of Wine Quality")
+    ax.set_xlabel('Col#4')
+    ax.set_ylabel('Col#5')
+    ax.set_zlabel('Col#6')
+    plt.legend()
+    plt.show()
--- PCA_KNN_Classification.py
+++ PCA_KNN_Classification.py
@@ -11,20 +11,20 @@ from Main_3_1 import myPca
 data_file = './DryBeanDataset/Dry_Bean_Dataset.xlsx'
 data = pd.read_excel(data_file, sheet_name='Dry_Beans_Dataset')
 
-#scramble the order of the database
+# scramble the order of the database
 data = data.sample(frac=1).reset_index(drop=True)
-#Change lables to numeric values
-data[['Class']] = data[['Class']].apply(lambda col:pd.Categorical(col).codes)
+# Change lables to numeric values
+data[['Class']] = data[['Class']].apply(lambda col: pd.Categorical(col).codes)
 
 data['Class'].unique()
 
-plt.figure(figsize=(12,6))
-sns.heatmap(data.corr(),annot=True)
+plt.figure(figsize=(12, 6))
+sns.heatmap(data.corr(), annot=True)
 
-plt.figure(figsize=(12,6))
+plt.figure(figsize=(12, 6))
 plt.title('Class Count')
 sns.set_style('white')
-sns.countplot(x='Class',data=data)
+sns.countplot(x='Class', data=data)
 
 
 # Split the data into train and test with 80 train / 20 test
@@ -33,17 +33,21 @@ Y = data["Class"]
 
 accuracy = []
 for i in np.arange(1, 16):
-    Mean_col, Std_col, X_meaned, eigenvalues, eigenvectors, X_projected = myPca(X,i)
-    X_train, X_test, y_train, y_test = train_test_split(X_projected, Y, train_size=0.75, random_state=0)
+    Mean_col, Std_col, X_normalized, eigenvalues, eigenvectors, X_projected = myPca(
+        X, i)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_projected, Y, train_size=0.75, random_state=0)
 
     model = KNeighborsClassifier(n_neighbors=3)
-    scores = cross_val_score(model, X_train, y_train, scoring="accuracy", cv=5)  # StratifiedKFold CV is being done
-    accuracy.append(scores.mean())  # we'll store the average value of accuracy calculated over all the 10 CV sets
+    # StratifiedKFold CV is being done
+    scores = cross_val_score(model, X_train, y_train, scoring="accuracy", cv=5)
+    # we'll store the average value of accuracy calculated over all the 10 CV sets
+    accuracy.append(scores.mean())
 
-plt.figure(figsize=(12,3))
+plt.figure(figsize=(12, 3))
 accuracy = np.array(accuracy)
-plt.plot(np.arange(1,16), 1-accuracy)
+plt.plot(np.arange(1, 16), 1-accuracy)
 plt.xlabel("n_neighbors")
 plt.ylabel("1 - accuracy")
-plt.xticks(np.arange(1,16))
+plt.xticks(np.arange(1, 16))
 plt.legend()
